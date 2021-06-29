94ab7acfc0b9d46cb6447fc7e44f04cc9a6c4e13
Lucy Donnelly <lucy.donnelly@autotrader.co.uk>
2021-06-29 09:14:05 +0100

    add feature to show complexity of password

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ const password = document.getElementById("password");
 
 
 
-password.addEventListener("keydown", () => {
-    console.log('sfsdfsdfsdf'); 
-    progressBar.value = Math.floor(Math.random() * 11);
+password.addEventListener("input", () => {
+    // console.log('sfsdfsdfsdf'); 
+    // progressBar.value = Math.floor(Math.random() * 11);
+    if(password.value.length > 8){
+        console.log("you got 8 characters!")
+        progressBar.value = 2
+    }
 
 })
