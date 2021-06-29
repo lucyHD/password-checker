ca35d1c23daceca5b6747115a45d7e70d1c69b42
Lucy Donnelly <lucy.donnelly@autotrader.co.uk>
2021-06-29 11:11:07 +0100

    add feature to show complexity rating of password

--- app.js
+++ app.js
@@ -1,14 +1,37 @@
 const progressBar = document.getElementById("security"); 
 const password = document.getElementById("password"); 
+const complexityRating = document.getElementById("complexityRating"); 
+
+//functions
+
+const updateComplexityRating =  (progressBarValue) => {
+
+    if(progressBarValue <= 4){
+        complexityRating.innerHTML = "Weak"
+    }else if(progressBarValue <= 7){
+        complexityRating.innerHTML = "Medium"
+    }else if(progressBar > 7){
+        complexityRating.innerHTML = "Strong"
+    }
+}
 
 
 
-password.addEventListener("input", () => {
-    // console.log('sfsdfsdfsdf'); 
+
+
+// event listener
+password.addEventListener("input", () => { 
+
     // progressBar.value = Math.floor(Math.random() * 11);
-    if(password.value.length > 8){
+    if(password.value.length >= 8){
         console.log("you got 8 characters!")
         progressBar.value = 2
+        updateComplexityRating(progressBar.value); 
+    // }
+    
+    // if(password.value.length > 8 && password.value.match(/[A-Z]+/g)){
+    //     console.log("you got 8 characters and an uppercase letter!")
     }
 
 })
+
